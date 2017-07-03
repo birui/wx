83ee35d891fe24f1246b1fc3b6f5f2dfdc274f7a
rocky <273848345@qq.com>
2017-07-03 10:22:48 +0800

    修改欢迎语

--- wxbot/LCBot/single_group.py
+++ wxbot/LCBot/single_group.py
@@ -118,10 +118,10 @@ def reply_text(wx_own):
 def group_Welcome(g_name):
     id_list = []
     try:
-        group_Welcome = Cron_msg.objects.filter(msg_group=g_name).values('msg_content')
+        group_Welcome_msg = Cron_msg.objects.filter(msg_group=g_name).values('msg_content')
     except Exception as e:
-        group_Welcome = Cron_msg.objects.filter(msg_group='default').values('msg_content')
-    for i in group_Welcome:
+        group_Welcome_msg = Cron_msg.objects.filter(msg_group='default').values('msg_content')
+    for i in group_Welcome_msg:
         id_list.append(i['msg_content'])
     return(random.choice(id_list))
 
